37545c9645481c19786752f96b0c914f08625a07
bmbdigitalproject-code <bmbdigitalproject@gmail.com>
2026-07-11 18:25:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const sponsor = "🎮 JOYSTICK BY REXUS • Official Gaming Gear Indonesia • Main Lebih Presisi, Main Lebih Nyaman";
 
+const berita = [
+"⚽ Timnas Indonesia terus mempersiapkan diri menghadapi agenda internasional.",
+"🏸 Indonesia menargetkan gelar juara di turnamen bulu tangkis dunia.",
+"🏍 Persaingan MotoGP semakin ketat memasuki seri berikutnya.",
+"🏀 NBA memasuki persiapan musim baru.",
+"⚽ Bursa transfer pemain Eropa masih menjadi sorotan.",
+"🏆 Liga 1 Indonesia menghadirkan persaingan yang semakin sengit."
+];
+
+let hasil = "";
+
+berita.forEach((item, index) => {
+    hasil += item + " • ";
+
+    if ((index + 1) % 3 === 0) {
+        hasil += sponsor + " • ";
+    }
+});
+
+document.getElementById("marquee").innerHTML = hasil.repeat(5);
